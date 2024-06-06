d33e59764d3daa783c7af82d811f6ad68954b504
Criss <crxstxxp@icloud.com>
2024-06-05 18:39:56 -0600

    refactor(products): create a product table

--- src/app/components/products/products-list/products-list.component.ts
+++ src/app/components/products/products-list/products-list.component.ts
@@ -1,10 +1,28 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ProductsServiceService } from '../../../services/products/products-service.service';
+import { Product } from '../../../models/Product';
 
 @Component({
   selector: 'app-products-list',
   templateUrl: './products-list.component.html',
-  styleUrl: './products-list.component.scss'
+  styleUrl: './products-list.component.scss',
 })
-export class ProductsListComponent {
+export class ProductsListComponent implements OnInit{
+  constructor(private productService: ProductsServiceService) {}
+
+  productList: Product[] = [];
+
+  async getProducts() {
+    try {
+      this.productService.getProducst().
+      subscribe(item => this.productList = item);
+    } catch (error) {
+      console.log(error);
+    }
+  }
+
+  ngOnInit(): void {
+    this.getProducts();
+  }
 
 }
